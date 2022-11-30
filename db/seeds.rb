566537eabd5850fb2ba78a31a0b6b26cfdaeadcc
Owusu-Desmond <desmond0406114009@gmail.com>
2022-11-30 02:06:03 +0000

    Create api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Greeting.create!( message: "Hello World!" )
+Greeting.create!( message: "Hey, How are you?" )
+Greeting.create!( message: "Good morning!, Hope you are doing well" )
+Greeting.create!( message: "Hi, How is your day going?" )
+Greeting.create!( message: "Good day!, How are you doing?" )
